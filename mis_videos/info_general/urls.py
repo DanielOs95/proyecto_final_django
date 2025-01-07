3bc10c1a7fc368fb70cb57ab960580576b715fd3
Daniel <espartan.95.os@gmail.com>
2025-01-07 15:08:50 -0600

    ultimoCommit

--- mis_videos/info_general/urls.py
+++ mis_videos/info_general/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import manejo_datos
 
 urlpatterns = [
-    path("principal", views.pagina_principal, name="principal"),
+    path("", views.pagina_principal, name="principal"),
     path('manejo_datos/', views.manejo_datos, name='manejo_datos'),
     path("bienvenida", views.bienvenida, name="bienvenida"),
     path("tercera/", views.tercera_pagina, name="tercera"),
